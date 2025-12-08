85150539726fb850fe36fabadc70e42962823f0a
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2025-12-08 16:28:42 +0700

    push praktikum 9

---
+++ Pertemuan09/Praktikum/P9_714240032/P9_714240032/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P9_714240032
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Tampil()
+        {
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+
+            // Mengubah Nama Kolom Tabel
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No HP";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tampil();
+        }
+    }
+}
