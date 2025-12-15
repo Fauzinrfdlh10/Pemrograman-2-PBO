938230584b555a46c690c72546aa6dc9f0d8452c
Octa Tirta Investment <email_akun_githubmu@example.com>
2025-12-15 15:05:46 +0700

    push praktikum 10

---
+++ Pertemuan10/Praktikum/P10_714240032/P9_714240032/controller/Koneksi.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace P9_714240032
+{
+    internal class Koneksi
+    {
+        string connectionstring = "Server=localhost;Database=pemrog2ulbi;Uid=root;Pwd=;";
+        MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            kon = new MySqlConnection(connectionstring);
+            kon.Open();
+        }
+
+        public void CloseConnection()
+        {
+            kon.Close();
+        }
+
+        public object ShowData(string query)
+        {
+            try
+            {
+                OpenConnection();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, kon);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                CloseConnection();
+                return table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+        public void ExecuteQuery(MySqlCommand command)
+        {
+            command.Connection = kon;
+            command.ExecuteNonQuery();
+        }
+
+    }
+}
---
+++ Pertemuan10/Praktikum/P10_714240032/P9_714240032/model/M_mahasiswa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_714240032.model
+{
+    internal class M_mahasiswa
+    {
+        string npm, nama, angkatan, alamat, email, no_hp;
+
+        public M_mahasiswa()
+        { 
+        
+        }
+
+        public M_mahasiswa(string npm, string nama, string angkatan, string alamat, string email, string no_hp)
+        {
+            this.Npm = npm;
+            this.Nama = nama;
+            this.Angkatan = angkatan;
+            this.Alamat = alamat;
+            this.Email = email;
+            this.No_hp = no_hp;
+        }
+
+        public string Npm { get => npm; set => npm = value; }
+        public string Nama { get => nama; set => nama = value; }
+        public string Angkatan { get => angkatan; set => angkatan = value; }
+        public string Alamat { get => alamat; set => alamat = value; }
+        public string Email { get => email; set => email = value; }
+        public string No_hp { get => no_hp; set => no_hp = value; }
+    }
+}
